ea05fabec471793f68f3f65d865f140b2f33812d
ikonkere@gmail.com <ikonkere@gmail.com@826405c2-a995-af14-0fcf-4531d227a51e>
2010-10-07 19:46:36 +0000

    RC2: annotation configuration support

---
+++ trunk/wrenched/backend/src/main/java/com/wrenched/core/annotations/LazyAttributeFetcher.java
@@ -0,0 +1,13 @@
+package com.wrenched.core.annotations;
+
+import static java.lang.annotation.ElementType.METHOD; 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention; 
+import java.lang.annotation.Target;
+
+@Target({METHOD}) @Retention(RUNTIME) 
+public @interface LazyAttributeFetcher { 
+        Class<?> targetClass(); 
+        String attributeName(); 
+} 
