b2347730718c48a6f0cc4ac2e95879f3b738d95a
ikonkere@gmail.com <ikonkere@gmail.com@826405c2-a995-af14-0fcf-4531d227a51e>
2010-10-07 19:47:18 +0000

    RC2: init-time class introspection support

---
+++ trunk/wrenched/backend/src/main/java/com/wrenched/core/services/support/ClassIntrospectionUtil.java
@@ -0,0 +1,165 @@
+package com.wrenched.core.services.support;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+
+import com.wrenched.core.domain.LazyAttributeRegistryDescriptor;
+
+public class ClassIntrospectionUtil {
+	private static abstract class ClassOperation<T> {
+		T result;
+		Class<?> clazz;
+
+		ClassOperation(T arg0, Class<?> arg1) {
+			result = arg0;
+			clazz = arg1;
+		}
+
+		abstract boolean isSatisfied(AccessibleObject arg0);
+
+		abstract void process(String arg0);
+
+		T introspect(boolean arg2) {
+			if (arg2) {
+				for (Field f : clazz.getDeclaredFields()) {
+					if (isSatisfied(f)) {
+						process(f.getName());
+					}
+				}
+			}
+			else {
+				for (Method m : clazz.getMethods()) {
+					if (isSatisfied(m)) {
+						process(toLowerCaseFirst(m.getName().replaceFirst("get", "")));
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+
+	public static Collection<LazyAttributeRegistryDescriptor> findClasses(String domain, boolean fieldAccess) {
+		Collection<LazyAttributeRegistryDescriptor> classes =
+			new ArrayList<LazyAttributeRegistryDescriptor>();
+
+		ClassPathScanningCandidateComponentProvider provider =
+			new ClassPathScanningCandidateComponentProvider(true);
+		provider.addIncludeFilter(new AnnotationTypeFilter(javax.persistence.Entity.class));
+
+		for (BeanDefinition component : provider.findCandidateComponents(domain)) {
+			LazyAttributeRegistryDescriptor descriptor = introspect(component.getBeanClassName(), fieldAccess);
+			
+			if (descriptor != null) {
+				classes.add(descriptor);
+			}
+		}
+
+		return classes;
+	}
+	
+	private static LazyAttributeRegistryDescriptor introspect(String className, boolean fieldAccess) {
+		try {
+			Class<?> cls = Class.forName(className);
+
+			LazyAttributeRegistryDescriptor d = new LazyAttributeRegistryDescriptor();
+			d.className = cls.getCanonicalName();
+			d.idName = findEntityIdName(cls, fieldAccess);
+			d.attributes =
+				new ClassOperation<Collection<String>>(new ArrayList<String>(), cls) {
+					@Override
+					public boolean isSatisfied(AccessibleObject arg0) {
+						return isDeclaredLazy(arg0);
+					}
+
+					@Override
+					public void process(String arg0) {
+						result.add(arg0);
+					}
+				}.introspect(fieldAccess);
+
+			if (!d.attributes.isEmpty()) {
+				return d;
+			}
+		}
+		catch (ClassNotFoundException cnfe) {
+		}
+		
+		return null;
+	}
+
+	private static Object findEntityIdName(Class<?> clazz, boolean fieldAccess) {
+		if (clazz.isAnnotationPresent(javax.persistence.IdClass.class)) {
+			LazyAttributeRegistryDescriptor d = new LazyAttributeRegistryDescriptor();
+			d.className = clazz.getAnnotation(javax.persistence.IdClass.class).value().getCanonicalName();
+			d.attributes =
+				new ClassOperation<Collection<String>>(new ArrayList<String>(), clazz) {
+					@Override
+					public boolean isSatisfied(AccessibleObject arg0) {
+						return isDeclaredId(arg0);
+					}
+	
+					@Override
+					public void process(String arg0) {
+						result.add(arg0);
+					}
+				}.introspect(fieldAccess);
+			return d;
+		}
+		else {
+			return new ClassOperation<String>("", clazz) {
+				@Override
+				public boolean isSatisfied(AccessibleObject arg0) {
+					return isDeclaredId(arg0);
+				}
+
+				@Override
+				public void process(String arg0) {
+					result = arg0;
+				}
+			}.introspect(fieldAccess);
+		}
+	}
+
+	private static boolean isDeclaredId(AccessibleObject a) {
+		return a.isAnnotationPresent(javax.persistence.Id.class)
+				|| a.isAnnotationPresent(javax.persistence.EmbeddedId.class);
+	}
+
+	private static boolean isDeclaredLazy(AccessibleObject a) {
+		javax.persistence.FetchType ft = null;
+
+		if (a.isAnnotationPresent(javax.persistence.Basic.class)) {
+			ft = a.getAnnotation(javax.persistence.Basic.class).fetch();
+		}
+		else if (a.isAnnotationPresent(javax.persistence.OneToOne.class)) {
+			ft = a.getAnnotation(javax.persistence.OneToOne.class).fetch();
+		}
+		else if (a.isAnnotationPresent(javax.persistence.OneToMany.class)) {
+			ft = a.getAnnotation(javax.persistence.OneToMany.class).fetch();
+		}
+		else if (a.isAnnotationPresent(javax.persistence.ManyToOne.class)) {
+			ft = a.getAnnotation(javax.persistence.ManyToOne.class).fetch();
+		}
+		else if (a.isAnnotationPresent(javax.persistence.ManyToMany.class)) {
+			ft = a.getAnnotation(javax.persistence.ManyToMany.class).fetch();
+		}
+
+		return (ft != null) && (ft == javax.persistence.FetchType.LAZY);
+	}
+
+	static String toUpperCaseFirst(String s) {
+		return String.valueOf(s.charAt(0)).toUpperCase().concat(s.substring(1, s.length()));
+	}
+
+	static String toLowerCaseFirst(String s) {
+		return String.valueOf(s.charAt(0)).toLowerCase().concat(s.substring(1, s.length()));
+	}
+}
--- trunk/wrenched/backend/src/main/java/com/wrenched/core/services/support/MethodBasedAttributeProvider.java
+++ trunk/wrenched/backend/src/main/java/com/wrenched/core/services/support/MethodBasedAttributeProvider.java
@@ -2,8 +2,13 @@ package com.wrenched.core.services.support;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.wrenched.core.annotations.LazyAttributeFetcher;
+import com.wrenched.core.domain.LazyAttributeRegistryDescriptor;
 
 /**
  * convenient method-invoking provider that would call certain methods to fetch lazy attributes,
@@ -12,7 +17,7 @@ import java.util.Map;
  * @author konkere
  *
  */
-public abstract class MethodBasedAttributeProvider extends AbstractAttributeProvider {
+public class MethodBasedAttributeProvider extends AbstractAttributeProvider {
 	public static final String SEPARATOR = "#";
 	private final Map<String, String> methods = new HashMap<String, String>();
 	
@@ -43,6 +48,35 @@ public abstract class MethodBasedAttributeProvider extends AbstractAttributeProv
 			}
 		}
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.wrenched.core.services.support.LazyAttributeProvider#getManagedClasses()
+	 */
+	public Collection<LazyAttributeRegistryDescriptor> getManagedClasses() {
+		Map<String, LazyAttributeRegistryDescriptor> classes =
+			new HashMap<String, LazyAttributeRegistryDescriptor>();
+		
+		for (String qualifiedName : this.methods.keySet()) {
+			String[] z = qualifiedName.split(SEPARATOR);
+
+			if (!classes.containsKey(z[0])) {
+				classes.put(z[0], new LazyAttributeRegistryDescriptor());
+			}
+			
+			classes.get(z[0]).className = z[0];
+			//TODO: somewhow get hold on id-name here?
+			classes.get(z[0]).idName = "self";
+			
+			if (classes.get(z[0]).attributes == null) {
+				classes.get(z[0]).attributes = new ArrayList<String>();
+			}
+			
+			classes.get(z[0]).attributes.add(z[1]);
+		}
+
+		return classes.values();
+	}
 
 	/**
 	 * a map of loader methods per domain className#attributeName that
@@ -53,6 +87,25 @@ public abstract class MethodBasedAttributeProvider extends AbstractAttributeProv
 	public void setMethods(Map<String, String> ms) {
 		for (String name : ms.keySet()) {
 			this.methods.put(this.getDomain() + "." + name, ms.get(name));
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.wrenched.core.services.support.AbstractAttributeProvider#setDelegate(java.lang.Object)
+	 */
+	public void setDelegate(Object delegate) {
+		super.setDelegate(delegate);
+
+		for (Method m : this.delegate.getClass().getDeclaredMethods()) {
+			if (m.isAnnotationPresent(LazyAttributeFetcher.class)) {
+				String className = m.getAnnotation(LazyAttributeFetcher.class).targetClass().getCanonicalName();
+				
+				if (className.startsWith(this.getDomain())) {
+					methods.put(className + SEPARATOR + m.getAnnotation(LazyAttributeFetcher.class).attributeName(),
+							m.getName());
+				}
+			}
 		}
 	}
 }
