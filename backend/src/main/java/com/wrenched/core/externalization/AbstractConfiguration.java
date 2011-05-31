4c86ee4adf63a04b3e74bea713c85816b76552f3
ikonkere <ikonkere@826405c2-a995-af14-0fcf-4531d227a51e>
2011-05-31 12:28:57 +0000

    1.1.0: further refactoring of reflection stuff

--- backend/src/main/java/com/wrenched/core/externalization/AbstractConfiguration.java
+++ backend/src/main/java/com/wrenched/core/externalization/AbstractConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.wrenched.core.externalization.Externalizer.Configuration;
 import com.wrenched.core.externalization.Externalizer.Converter;
+import com.wrenched.core.services.support.ClassIntrospectionUtil.HierarchyTraverser;
 
 /**
  * 
@@ -16,64 +17,18 @@ import com.wrenched.core.externalization.Externalizer.Converter;
 public abstract class AbstractConfiguration implements Configuration {
 	protected static final Map<Class, Converter> converters = new HashMap<Class, Converter>();
 
+	private static final HierarchyTraverser<Converter> TRAVERSER = new HierarchyTraverser<Converter>() {
+		@Override
+		protected Converter processMatch(Class clazz) {
+			return converters.get(clazz);
+		}
+	};
+	
 	/*
 	 * (non-Javadoc)
 	 * @see com.wrenched.core.externalization.Externalizer.Configuration#findConverter(java.lang.Class)
 	 */
 	public Converter findConverter(Class clazz) {
-		/*
-		Converter c = null;
-		Class tmp = clazz;
-		
-		while (c == null && tmp != null) {
-			c = converters.get(tmp);
-			tmp = tmp.getSuperclass();
-		}
-		
-		return c;
-		*/
-		
-		return traverseSuperclass(clazz);
-	}
-
-	/**
-	 * iteratively searches for a converter by going up the superclasses
-	 * @param clazz
-	 * @return
-	 */
-	protected Converter traverseSuperclass(Class clazz) {
-		Converter c = null;
-		Class tmp = clazz;
-		
-		while (c == null && tmp != null) {
-			c = traverseInterfaces(tmp);
-			tmp = tmp.getSuperclass();
-		}
-		
-		return c;
-	}
-
-	/**
-	 * recursively searches for a converter amongst the
-	 * tree of interfaces. note that there's no particular order
-	 * of interfaces and therefore no guaranty of finding a particular one
-	 * if several converters potentially exist.
-	 * @param clazz
-	 * @return
-	 */
-	protected Converter traverseInterfaces(Class clazz) {
-		Converter c = converters.get(clazz);
-		
-		if (c != null) {
-			return c;
-		}
-		
-		Class[] is = clazz.getInterfaces();
-		
-		for (int i = 0; i < is.length && c == null; i++) {
-			c = traverseInterfaces(is[i]);
-		}
-		
-		return c;
+		return TRAVERSER.traverse(clazz);
 	}
 }
